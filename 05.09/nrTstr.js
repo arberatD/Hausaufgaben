870c123a9f590c110cfae2bf42a38dca5c8b3336
arberatD <arberatdushi@gmail.com>
2023-09-05 16:04:59 +0200

    Number to String

---
+++ 05.09/nrTstr.js
@@ -0,0 +1,9 @@
+let nummer = 9
+let string = "";
+
+// übergabe des wertes Nummer an string und Konvertierung mit toString()
+string = toString(nummer);
+
+// Ausgabe des Typen
+console.log(`Der Wert von nummer ist ${typeof nummer}. 
+Der Wert von string ist ${typeof string}.`)
