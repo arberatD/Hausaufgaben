419bbe1818c5ef1b4ab5bc98411ef79f78ad4c5a
arberatD <arberatdushi@gmail.com>
2023-09-05 17:13:32 +0200

    multiply lesson

---
+++ 05.09/multipliziere.js
@@ -0,0 +1,10 @@
+// angabe der Zahlen
+zahl1 = 5
+zahl2 = 3
+// function zum multiplizieren der zahlen1 und 2
+function multipliziere (zahl1, zahl2) {
+    // formatierung der Ausgabe
+  return `Wenn man ${zahl1} mit ${zahl2} multipliziert erhält man ${zahl1 * zahl2}`
+}
+// ausgabe
+console.log(multipliziere(zahl1, zahl2))
