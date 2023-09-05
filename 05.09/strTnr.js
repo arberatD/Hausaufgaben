f87e4fa4ac41ae18ee784707bd3be6b1ff40326e
arberatD <arberatdushi@gmail.com>
2023-09-05 15:46:14 +0200

    typeof Aufgabe

---
+++ 05.09/strTnr.js
@@ -0,0 +1,11 @@
+// erstellen eines String 
+let zahlAlsText = "9";
+
+// Ausgabe des Strings
+console.log(typeof zahlAlsText);
+
+// Ändern des Strings zu Number mit dem Objekt Number
+zahlAlsText = Number(zahlAlsText)
+
+// Ausgabe des neuen Werts
+console.log(typeof zahlAlsText)
